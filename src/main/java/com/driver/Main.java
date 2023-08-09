e80db0a683fae4f8f1aee8dcb34cb4fb8ba08926
Rahul Kishne <kishnerahul123@gmail.com>
2023-08-09 12:13:22 +0530

    adding solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,4 +8,18 @@ public class Main {
         p.product(4,8);
     }
 
+    public static class Product {
+        public int product(int x, int y) {
+            int a = x + y;
+            return a;
+        }
+        public int product(int x,int y,int z){
+            int sum=x+y+z;
+            return sum;
+        }
+        public double product(double x,double y){
+            double c=x+y;
+            return c;
+        }
+    }
 }
